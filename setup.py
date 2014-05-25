303a17985c436e34dc103099e9df3a787dec90a6
Niklas P <contact@niklasplessing.net>
2014-05-25 14:08:56 +0200

    repo url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 setup(
     name='wordcloud',
     version='1.0.0',
-    url='https://github.com/amueller/word_cloud',
+    url='https://github.com/niklasp/word_cloud',
     license='MIT',
     ext_modules=cythonize('wordcloud/query_integral_image.pyx'),
     packages=['wordcloud'],
