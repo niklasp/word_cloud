68713bda67dc78b70fb6e82a71df27b12d809222
Niklas P <contact@niklasplessing.net>
2014-05-27 21:09:56 +0200

    try for pip installs

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+ext_modules = [ ]
+
+if use_cython:
+    ext_modules += [
+        Extension("query_integral_image", [ 'wordcloud/query_integral_image.pyx' ]),
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("query_integral_image", [ 'wordcloud/query_integral_image.c' ]),
+    ]
 
 setup(
     name='wordcloud',
     version='1.0.0',
     url='https://github.com/niklasp/word_cloud',
     license='MIT',
-    ext_modules=cythonize('wordcloud/query_integral_image.pyx'),
+    cmdclass= cmdclass,
+    ext_modules=ext_modules,
     packages=['wordcloud'],
     package_data={'wordcloud': ['stopwords']}
 )
